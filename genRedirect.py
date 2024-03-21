047b0f45ecc6e4d751323e4b45124377b91961ce
shravan0553 <54292292+shravan0553@users.noreply.github.com>
2024-03-21 16:33:53 -0700

    Update genRedirect.py

--- genRedirect.py
+++ genRedirect.py
@@ -4,6 +4,6 @@ column_headers = ["MatchUrlPrefix", "RedirectUrl", "StatusCode", "Enabled"]
 with open('redirect_csv_file.csv', 'w') as csvf:
     writer = csv.writer(csvf)
     writer.writerow(column_headers)
-    for _ in range(50):
+    for _ in range(500000):
         row = ["/ip/"+str(i)+"/", "/i/", 301, True]
         writer.writerow(row)
